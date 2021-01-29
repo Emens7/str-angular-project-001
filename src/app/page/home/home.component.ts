380d8feead726c13e8be6511badc8ee5bba67d4f
Emens7 <magyaristvanlaszlo@gmail.com>
2021-01-29 12:04:26 +0100

    Home component 5 featured true random

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductServiceService } from '../../service/product-service.service';
+import { Product } from '../../model/product';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  products = [];
+
+  constructor(private productService: ProductServiceService) { }
 
   ngOnInit(): void {
+    this.products = this.productService.homeFeaturedFive();
   }
 
 }
